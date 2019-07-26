3a8176fe3bd03c6b2317614f6ccbd4a59c5a4083
artem <esipovich92@gmail.com>
2019-07-26 11:34:30 -0500

    Added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedPage = 'recipes';
 
-  onNavigate(page: string) {
-    this.loadedPage = page;
-  }
 }
