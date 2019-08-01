34370cb5e2eae6e0780eb8bf4bd44ace825246f1
artem <esipovich92@gmail.com>
2019-08-01 12:33:10 -0500

    Replaced EventEmitter with Subject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('Khachapuri', 'Traditional georgian dish',
       'https://prods3.imgix.net/images/articles/2017_10/nonfeatured-what-is-khachapuri.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=670',
